c21a29c219fab417949a31182290e09ba37a3103
Ruchita Sagalgile <56263059+ruchita00@users.noreply.github.com>
2023-09-30 18:06:56 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,55 @@
+// let score= "33abc";
+let score=true;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+//null ke case me type conversion of number is coming =0
+//undefined ke case me type conversion of number is coming =NAN
+//string"ruchi" ke case me type conversion of number is coming =NAN
+//boolean value true ke case me type conversion of number is 
+//coming =1
+
+//number
+//"33" =>33
+//"33abc" =>NAN
+//true =>1; false =>0;
+
+
+//boolean value false ke case me type conversion of number is 
+//coming =1
+
+
+//jab hum type conversion krte hai
+//string into number tb uska value bhi check kre 
+//kahi  vo nan to nai hai
+
+
+
+
+
+let isLoggedIn=1;
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//boolean 
+//1 =>true
+//0 => false
+//"" =>false
+//"ruchi" =>true
+
+
+let someNumber= 33;
+
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//string
+//33 => 33  string
