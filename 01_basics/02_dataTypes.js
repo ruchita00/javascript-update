e8e22f540792150fef52494c9fa9ff0fd46076fa
Ruchita Sagalgile <56263059+ruchita00@users.noreply.github.com>
2023-09-30 17:14:33 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict" //treat all js code as newer version
+
+// alert(3+3) // we can not use alert in node js 
+// we are not using nodejs not broswer
+//javascript ka  engine he vo browser me chupa hua he
+
+// console.log(3+3); //cod readablity should be high
+
+// console.log("hitesh");
+
+let name="ruchi";
+let age="25"
+let isLoggedIn=false;
+
+//primitive datatype
+
+//number ==2 to power 53
+//if number bhoht jada bada hai to bigint use krte he
+//bigint
+//string === ""
+//boolean == true/false
+//null = standalone  value == mtlb filhal ye khali hai 
+// ex  temperature == starting me 0 ni krte kyoki temp 0 bhi value he 
+//isliye hum null bejte he taki temperature khali hai filhal
+//undefined==> 
+//symbol == unique
+
+//object
+
+console.log(typeof "ruchita");
+
+//null is an object
+console.log(typeof null);  // object
+
+console.log(typeof undefined); // undefined
+
