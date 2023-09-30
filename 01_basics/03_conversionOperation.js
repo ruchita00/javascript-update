6e1a05df2029bdf61856f9086d726e08a6f85b10
Ruchita Sagalgile <56263059+ruchita00@users.noreply.github.com>
2023-09-30 18:35:46 +0000

    camparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -48,8 +48,67 @@ let booleanIsLoggedIn=Boolean(isLoggedIn);
 let someNumber= 33;
 
 let stringNumber=String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 //string
 //33 => 33  string
+
+// *********** Operations ***************
+
+let value =3;
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello"
+let str2=" ruchi"
+// let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+
+// console.log("1"+2+2);
+
+// console.log(1+2+"2");
+
+// console.log((3+4) *5 %3);
+
+
+// console.log(+true); =>1
+// console.log(+""); =>0
+
+
+
+let num1,num2,num3;
+
+num1=num2=num3=2+2;
+
+
+//prefix and post fix
+
+let gameCounter=100
+// gameCounter++
+// ++gameCounter
+// console.log(gameCounter);
+
+let x=3;
+const y=x++;
+
+console.log(`x:${x}, y:${y}`); //x=4,y=3
+
+let a=3;
+const b=++a;
+
+console.log(`a:${a},b:${b}`);
+
+
+
